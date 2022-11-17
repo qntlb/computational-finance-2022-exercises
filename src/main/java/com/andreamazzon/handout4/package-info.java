eaefa5d33919ec3de1e05ebf96a6d8ce227481ff
Andrea Mazzon <mazzon@math.lmu.de>
2022-11-17 18:09:51 +0100

    Solutions of Handout 4

---
+++ src/main/java/com/andreamazzon/handout4/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package contains the code for the solution of the exercises of the fourth
+ * handout. In particular, we see the implementation of the pricing of a Caplet and
+ * of a Quanto Caplet under the Black model, and a theoretical exercise about
+ * the pricing of a Quanto Caplet under the Bachelier model for the Libor rate in foreign
+ * currency.
+ * 
+ * @author Andrea Mazzon
+ */
+package com.andreamazzon.handout4;
